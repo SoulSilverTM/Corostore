8061a012fb32c31f96d83789fe70c923ffac216a
SoulSilverTM <134556047+SoulSilverTM@users.noreply.github.com>
2023-05-24 16:15:28 -0600

    Add files via upload

---
+++ precio.py
@@ -0,0 +1,7 @@
+print("        programa para calcular el perimetro y la superficie") 
+altura=float(input("ingresa la altura  "))
+base=float(input("ingresa la base  "))
+perimetro=2*(altura+base)
+superficie=altura*base
+print("tu perimetro es  ", perimetro )
+print("tu superficie es  ", superficie )
